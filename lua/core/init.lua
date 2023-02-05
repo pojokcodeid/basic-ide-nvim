6f78935f1f099fec50798bf1c867ef436a284c68
pojok code <pojokcodeid@gmail.com>
2023-02-05 08:00:19 +0700

    update format on save

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -4,3 +4,7 @@ require("core.autocommands")
 require("core.keymaps")
 require("config.bufferline")
 require("config.snip")
+local format_onsave = true
+if format_onsave then
+	require("core.format_onsave")
+end
